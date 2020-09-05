b8d5f084e9923bd3d8366ddde899b0a91b602d80
orlando-messias <programadorlando@gmail.com>
2020-09-05 19:05:08 -0300

    landing page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Landing from './components/pages/Landing';
+import Landing from './pages/Landing';
 
 function Routes() {
   return (
