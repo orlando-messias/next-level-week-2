9e1800b0f45990ed22e46dc1259f82632277256e
orlando-messias <programadorlando@gmail.com>
2020-09-05 21:43:55 -0300

    teacher list and page header

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageHeader from '../PageHeader';
+import './teacherListStyles.css';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os Proffys disponíveis" />
+    </div>
+  );
+}
+
+export default TeacherList;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
 
 function Routes() {
   return (
     <Router>
       <Route exact path="/" component={Landing} />
+      <Route path="/study" component={TeacherList} />
     </Router>
   );
 }
