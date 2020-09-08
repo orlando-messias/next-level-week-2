68a11d77ea2e6f399f3b2368bb0a7f098f2952da
orlando-messias <programadorlando@gmail.com>
2020-09-08 10:54:46 -0300

    create component Input

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import PageHeader from '../PageHeader';
+import './teacherFormStyles.css';
+import Input from '../../components/Input';
 
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!" />
+      <PageHeader
+        title="Que incrível que você quer dar aulas!"
+        description="O primeiro passo é preencher este formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+
+        </fieldset>
+      </main>
+
     </div>
   );
 }
